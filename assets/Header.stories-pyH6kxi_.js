d62e0517b02288e65d63eed3ce0848f095ca1a47
hyunho0420-dot <hyunho0420@gmail.com>
2025-09-19 14:41:22 +0900

    Updates

---
+++ assets/Header.stories-pyH6kxi_.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-Cz2qKgN8.js";import"./iframe-DU5fVlMp.js";import"./preload-helper-PPVm8Dsz.js";import"./Button-DFZ_fkSR.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,m={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,m as default};
